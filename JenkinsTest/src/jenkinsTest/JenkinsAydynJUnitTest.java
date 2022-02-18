401ceb229d8bd0c87d7610cb1da04d9f9c5c0812
Aydynbek <badawest711@gmail.com>
2022-02-18 00:08:48 -0500

    Added a JUnit test

---
+++ JenkinsTest/src/jenkinsTest/JenkinsAydynJUnitTest.java
@@ -0,0 +1,20 @@
+package jenkinsTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JenkinsAydynJUnitTest {
+
+	@Test
+	public void test() {
+		int a = 1234;
+	    int b = 5678;
+	    int actual = a + b;
+	 
+	    int expected = 6912;
+	 
+	    assertEquals("eerewr wer ewrw", expected, actual);
+	}
+
+}
