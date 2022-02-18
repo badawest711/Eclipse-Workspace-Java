203f8e2c189c3857ee88ef7640c7147f88dbad9c
Aydynbek <badawest711@gmail.com>
2022-02-18 13:31:11 -0500

    Improved the build.xml file so that the ant command runs correctly in CMD

--- TestJunitWithAnt/src/testJunitWithAnt/TestMessageUtil.java
+++ TestJunitWithAnt/src/testJunitWithAnt/TestMessageUtil.java
@@ -7,19 +7,21 @@ import org.junit.Test;
 public class TestMessageUtil {
 
 	String message = "Robert";	
-   MessageUtil messageUtil = new MessageUtil(message);
-   
+
    @Test
    public void testPrintMessage() {	
+	   MessageUtil messageUtil = new MessageUtil(message);
+	   
       System.out.println("Inside testPrintMessage()");     
       assertEquals(message,messageUtil.printMessage());
    }
 
    @Test
    public void testSalutationMessage() {
+	   MessageUtil messageUtil = new MessageUtil(message);
+	   
       System.out.println("Inside testSalutationMessage()");
       message = "Hi!" + "Robert";
       assertEquals(message,messageUtil.salutationMessage());
    }
-
 }
